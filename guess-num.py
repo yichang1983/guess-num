145b209b4d7d64cc238bb41678f93e39e6bc74ea
Yi-Chang Chen <yichang.thomas@gmail.com>
2021-06-15 11:52:39 +0100

    final version

--- guess-num.py
+++ guess-num.py
@@ -1,6 +1,10 @@
 import random
+start = input('random number starts: ')
+end = input('random number ends: ')
+start = int(start)
+end = int(end)
 
-r = random.randint(1, 100)
+r = random.randint(start, end)
 count = 0
 while True:
 	count += 1		#count = count + 1
